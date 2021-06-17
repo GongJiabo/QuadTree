825f98d4c19ad659c2cb422ad8c672ea1f937977
正趣 <jbgong@dingtalk.com>
2021-06-17 20:31:50 +0800

    Initial Commit

---
+++ mytask/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  mytask
+//
+//  Created by jbgong on 2021/6/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
